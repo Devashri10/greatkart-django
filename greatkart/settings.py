c58a579c5ffa7f079eaaebff73e47a1c1bcbbae8
DevashriBhosale <bhosaledevashri1042@gmail.com>
2023-10-07 23:25:15 +0530

    handled cart and checkout page for the logged in users

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,17 @@ STATICFILES_DIRS = [
 #media file configuration
 MEDIA_URL ='/media/'
 MEDIA_ROOT =BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    50: "critical",
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bhosaledevashri1042@gmail.com'
+EMAIL_HOST_PASSWORD = 'sulz urfn nkmn iohw'
+EMAIL_USE_TLS = True
